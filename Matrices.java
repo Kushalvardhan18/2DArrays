3fe7dc6e1536486f5ebb59f920ade2411a41792e
Kushal Vardhan <vardhank140102@gmail.com>
2024-06-06 09:39:51 +0530

    Update Matrices.java

--- Matrices.java
+++ Matrices.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Matrices {
-    public static void main(String[] args){
-        int[][] matrix =new int [3][3];
+    public static void main(String[] args) {
+        int[][] matrix = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
         Scanner sc = new Scanner(System.in);
-        int n=3,m=3;
-        for (int i =0;i<n;i++){
-            for (int j = 0;j<m;j++){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 matrix[i][j] = sc.nextInt();
             }
-        }  for (int i =0;i<n;i++){
-            for (int j = 0;j<m;j++){
-                System.out.print( matrix[i][j] +" ");
-
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
