d63ac9fc823b78a4122c44b910c13202316a7d14
Kushal Vardhan <vardhank140102@gmail.com>
2024-06-06 09:48:16 +0530

    Searching in 2D Arrays

--- Matrices.java
+++ Matrices.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 
 public class Matrices {
+    public static boolean Search(int matrix[][],int key){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key){
+                    System.out.print("found at cell (" + i +"," + j +")");
+                    return true;
+                }
+            }
+        }
+            System.out.println("Key not found");
+                return false;
+    }
     public static void main(String[] args) {
         int[][] matrix = new int[3][3];
         int n = matrix.length, m = matrix[0].length;
@@ -16,5 +28,7 @@ public class Matrices {
             }
             System.out.println();
         }
+
+        Search(matrix,5);
     }
 }
