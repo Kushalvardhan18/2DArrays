bfe051b34d46ac8093e5d2d55444d65c872914c4
Kushal Vardhan <vardhank140102@gmail.com>
2024-06-11 10:38:40 +0530

    Update DiagonalSum.java

--- DiagonalSum.java
+++ DiagonalSum.java
@@ -3,23 +3,30 @@ public class DiagonalSum {
         int priSum = 0, secSum=0;
         int n = matrix.length, m = matrix[0].length;
         if (n == m) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (i == j) {
-                        priSum += matrix[i][j];
-                    }
-                    if (i+j == n-1){
-                        secSum += matrix[i][j];
-                    }
+//            for (int i = 0; i < n; i++) {
+//                for (int j = 0; j < m; j++) {
+//                    if (i == j) {
+//                        priSum += matrix[i][j];
+//                    }
+//                    else if (i+j == n-1){
+//                        secSum += matrix[i][j];
+//                    }
+//                }
+//            }
+            for (int i =0;i<m;i++){
+                priSum += matrix[i][i];
+                if(i != n-1-i) {
+                    secSum += matrix[i][n - 1 - i];
                 }
             }
+
             System.out.println("The sum of primary diagonal : " + priSum);
             System.out.print("The sum of secondary diagonal : " + secSum);
         }
     }
 
     public static void main(String[] args) {
-        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int[][] matrix = {{1, 2, 3}, {5, 6, 7}, {9, 10, 11,}};
         sumDiagonal(matrix);
     }
 }
