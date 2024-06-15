91328226b8fa6ef39dd0c0c84dcaaf5bc0c1fca7
Kushal Vardhan <vardhank140102@gmail.com>
2024-06-15 21:34:40 +0530

    Update PracticeQues.java

--- PracticeQues.java
+++ PracticeQues.java
@@ -13,11 +13,22 @@ public class PracticeQues {
 
     }
 
- 
+    public static void printSum(int num[][]) {
+        int sum = 0;
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[0].length; j++) {
+                if (i == 1) {
+                    sum = sum + num[i][j];
+                }
+            }
+            System.out.println(sum);
+        }
+    }
 
     public static void main(String[] args) {
         int[][] nums = {{4, 7, 8}, {8, 8, 7}};
+        int[][] num = { {1,4,9},{11,4,3},{2,2,3} };
         occurenceOfNumber(nums, 7);
-        printSum(nums);
+        printSum(num);
     }
 }
